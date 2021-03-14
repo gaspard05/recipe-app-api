00537bcff58631732ca40aa0904d161c0a760a85
Gaspard Nzamurambaho <gnzamura@hotmail.com>
2021-03-14 02:24:22 +0000

    Removing trailing whitespace

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -258,4 +258,4 @@ class RecipeImageUploadTests(TestCase):
         serializer3 = RecipeSerializer(recipe3)
         self.assertIn(serializer1.data, res.data)
         self.assertIn(serializer2.data, res.data)
-        self.assertNotIn(serializer3.data, res.data)    
+        self.assertNotIn(serializer3.data, res.data)
